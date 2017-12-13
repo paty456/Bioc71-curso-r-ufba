ee0865914f0aa245a33c2ef17ac18771a6346b88
patricia <patiimota@gmail.com>
2017-12-13 17:41:26 -0300

    script 8 /tutorial e exercicio

--- Aula 8/script8.r
+++ Aula 8/script8.r
@@ -2,8 +2,11 @@
 #7a. Regressao Linear Simples
 #Ajuste e diagnostico de uma regressao
 
-library(MASS)
-data(Animals)
-str(Animals)
+library(MASS) #pacote de dados de massa
+data(Animals) #dados sobre o objeto animals
+str(Animals) ##avalia a estrutura do objeto
 
 plot(brain~body, data=Animals)  #plot de y cerebro e x corpo dos dados dos animais
+#N foi linear
+plot(brain~body,data=Animals, log="xy") #transforma os dados em seus logaritmos, dentro da função plot
+plot(log(brain)~log(body), data=Animals) #nomea o y em log brain e x em log body plotando os valores transformados em log
